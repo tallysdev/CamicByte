c98245538ad673540707d603cf02943ceee24deb
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-05-22 23:07:22 -0300

    adicionando comentatios no script e no index

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
+// deficindo vareavel video
 const video = document.querySelector('#video');
 
+// função de iniciar Camera
 async function initCamera(){    
     if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
         navigator.mediaDevices.getUserMedia({video: true})
@@ -13,11 +14,14 @@ async function initCamera(){
     }
 }
 
+// função de parar camera passando video como null
 function pararCamera(){
     video.srcObject = null
 }
 
+// definindo varialvel
 const audio = document.querySelector('#audio');
+// função de iniciar microfone
 async function initMicrofone(){    
     if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
         navigator.mediaDevices.getUserMedia({audio: true})
@@ -30,16 +34,19 @@ async function initMicrofone(){
         })
     }
 }
+// função de parar microfe passando audio como null
 function pararMic(){
     audio.srcObject = null;
 }
     
-
+// iniciando variaveis de onoff e estado para a função do botão toggle
 var onoff = document.getElementById('onoff1');
 var estado = document.getElementById('estado');
 
+// função para o botao toggle
 onoff.addEventListener('change', function() {
     estado.innerHTML = this.checked ? 'Ligar' : 'Desligar';
+    // se botao estiver ligado entao vai iniciar a Camera, e se ele estiver desligado vai fechar a camera
     if (estado.innerHTML=='Ligar') {
         initCamera();
     }
@@ -48,10 +55,13 @@ onoff.addEventListener('change', function() {
     }
 });
 
+// iniciando e definindo vareiaveis do segundo botão toggle
 var onoffmic = document.getElementById('onoffmic');
 var estadomic = document.getElementById('estadomic');
 
+// função do segundo botao toggle que é o de mic
 onoffmic.addEventListener('change', function() {
+    // se botao estiver ligado entao vai iniciar o microfone, e se ele estiver desligado vai fechar o microfone
     estadomic.innerHTML = this.checked ? 'Ligar' : 'Desligar';
     if (estadomic.innerHTML=='Ligar') {
         initMicrofone();
